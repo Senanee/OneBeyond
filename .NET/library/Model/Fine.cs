d945baef67d57272c024b15da5eb446452b300b0
Senanee <senanee.w@gmail.com>
2025-03-15 19:35:50 +0000

    Adding Fine model and creating relationships

---
+++ .NET/library/Model/Fine.cs
@@ -0,0 +1,13 @@
+﻿namespace OneBeyondApi.Model
+{
+    public class Fine
+    {
+        public Guid Id { get; set; }
+        public Guid BorrowerId { get; set; }
+        public Borrower Borrower { get; set; }
+        public Guid BookId { get; set; }
+        public BookStock BookStock { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DateIssued { get; set; }
+    }
+}
