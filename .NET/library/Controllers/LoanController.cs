822516cb2c1754e9a23ba99aaf8f5de20894e34a
Senanee <senanee.w@gmail.com>
2025-03-15 15:48:19 +0000

    adding the OnLoan endpoint

---
+++ .NET/library/Controllers/LoanController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OneBeyondApi.DataAccess;
+using OneBeyondApi.Model;
+
+namespace OneBeyondApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoanController : ControllerBase
+    {
+        private readonly ILogger<LoanController> _logger;
+        private readonly ILoanRepository _loanRepository;
+
+        public LoanController(ILogger<LoanController> logger, ILoanRepository loanRepository)
+        {
+            _logger = logger;
+            _loanRepository = loanRepository;
+        }
+
+        [HttpGet]
+        [Route("OnLoan")]
+        
+        public async Task<ActionResult<IList<LoanDetail>>> GetActiveLoans()
+        {
+            var activeLoans=  await _loanRepository.GetActiveLoans();
+
+            if (!activeLoans.Any())
+                return NotFound("No active loans available");
+            return Ok(activeLoans);
+        }
+    }
+}
