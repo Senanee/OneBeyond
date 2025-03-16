d2e7e0350fc6547e7ba80155d97635fcccac9e00
Senanee <senanee.w@gmail.com>
2025-03-16 13:56:36 +0000

    Adding Reservation repository interface and implementations.

---
+++ .NET/library/DataAccess/IReservationRepository.cs
@@ -0,0 +1,13 @@
+﻿using OneBeyondApi.Model;
+
+namespace OneBeyondApi.DataAccess
+{
+    public interface IReservationRepository
+    {
+        Task<List<Reservation>> GetReservationsByBookId(Guid bookId);
+
+        Task<Response> AddReservation(Reservation reservation);
+
+        Task<Reservation> GetLatestReservationAsync(Guid bookId);
+    }
+}
---
+++ .NET/library/DataAccess/ReservationRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OneBeyondApi.Model;
+
+namespace OneBeyondApi.DataAccess
+{
+    public class ReservationRepository : IReservationRepository
+    {
+        public async Task<Response> AddReservation(Reservation reservation)
+        {
+            using (var context = new LibraryContext())
+            {
+                var currentEntity = context.Reservations.Add(reservation).Entity;
+                await context.SaveChangesAsync();
+
+                bool success = !(currentEntity is null || currentEntity.Id == Guid.Empty);
+
+                return new Response(success, success
+                    ? $"Reservation created successfully on {currentEntity.DateReserved} expected collection date {currentEntity.DateOfExpectedCollection}."
+                    : "Error occurred while making the reservation");
+            }
+        }
+
+        public async Task<List<Reservation>> GetReservationsByBookId(Guid bookId)
+        {
+            using (var context = new LibraryContext())
+            {
+                return await context.Reservations
+                    .Where(r => r.BookId == bookId).IgnoreAutoIncludes()
+                    .ToListAsync();
+            }
+        }
+
+        public async Task<Reservation> GetLatestReservationAsync(Guid bookId)
+        {
+            using (var context = new LibraryContext())
+            {
+                if (!context.Reservations.Any(x => x.BookId == bookId))
+                    return null!;
+                return await context.Reservations
+                    .Where(r => r.BookId == bookId)
+                    .OrderByDescending(x => x.DateReserved)
+                    .FirstAsync();
+            }
+        }
+
+    }
+}
